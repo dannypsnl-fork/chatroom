03827c38f822f8ef52d0e058f12bbb627e391846
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2021-06-09 23:49:12 +0800

    channel and socket

---
+++ lib/chatroom_web/channels/water_cooler_channel.ex
@@ -0,0 +1,16 @@
+defmodule ChatroomWeb.WaterCoolerChannel do
+  use ChatroomWeb, :channel
+
+  @impl true
+  def join("water_cooler:lobby", _payload, socket) do
+    {:ok, socket}
+  end
+
+  # It is also common to receive messages from the client and
+  # broadcast to everyone in the current topic (water_cooler:lobby).
+  @impl true
+  def handle_in("shout", payload, socket) do
+    broadcast(socket, "shout", payload)
+    {:noreply, socket}
+  end
+end
